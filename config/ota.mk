45b20fe18fb55fbcc4fe1b836841decf7882a677
KostyaJRZ <jrzroyale@gmail.com>
2020-01-31 08:29:35 +0000

    Don't build OTA Again :)

--- config/ota.mk
+++ config/ota.mk
@@ -5,8 +5,8 @@ CUSTOM_OTA_VERSION_CODE := ten
 CUSTOM_PROPERTIES += \
     org.pixelplusui.ota.version_code=$(CUSTOM_OTA_VERSION_CODE)
 
-PRODUCT_PACKAGES += \
-    Updates
+# PRODUCT_PACKAGES += \
+   # Updates
 
 PRODUCT_COPY_FILES += \
     vendor/aosp/config/permissions/org.pixelplusui.ota.xml:system/etc/permissions/org.pixelplusui.ota.xml
